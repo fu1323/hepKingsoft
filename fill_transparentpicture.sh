480309d7340ac0f85c334d973ebc6ffdf892e968
fu1323 <112527082+fu1323@users.noreply.github.com>
2026-04-23 13:47:52 +0800

    更新 fill_transparentpicture.sh

--- fill_transparentpicture.sh
+++ fill_transparentpicture.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+#(ai生成) 用来填充爬取word时下载的透明背景的图片为白色背景
 find folder -name "*.png" -type f -exec sh -c 'mkdir -p "output/$(dirname "${1#folder/}")"; magick "$1" -background white -flatten -alpha off "output/${1#folder/}"' _ {} \;
